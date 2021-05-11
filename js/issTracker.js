06c5ec7d068a51683574d54959441ff8ff75abba
Joey Thompson <joeythompson1014@gmail.com>
2021-05-11 10:43:02 -0400

    Formatted Speed with ',' separators

--- js/issTracker.js
+++ js/issTracker.js
@@ -1,20 +1,10 @@
 
-/*Get Who is in Space
-$.getJSON('https://api.open-notify.org/astros.json?callback=?', function(data) {
-    var number = data['number'];
-    $('#spacepeeps').html("There are currently " + number + " people in space");
-
-    data['people'].forEach(function (d) {
-         $('#astronames').append('<li>' + d['name'] + '</li>');
-    });
-});
-*/
 
 //Leaflet.js Map Setup
 const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const icon = L.icon({
-  iconUrl: "https://raw.githubusercontent.com/jlthompson96/NASA-API-Website/126ca8b04448b3792df1b36695d98f86c8bf9095/assets/iss.svg",
+  iconUrl: "../assets/iss.svg",
   iconSize:[175,175]
 });
 const issMarker = L.marker([0,0],{icon:icon}).addTo(mymap);
@@ -36,9 +26,10 @@ async function getISSInfo(){
   const{latitude,longitude, velocity, altitude} = data;
   mymap.setView([latitude,longitude],3);
   issMarker.setLatLng([latitude,longitude]);
+  const formatter = new Intl.NumberFormat('en');
   document.getElementById('lat').textContent = latitude.toFixed(4);
   document.getElementById('lng').textContent = longitude.toFixed(4);
-  document.getElementById('velocity').textContent = velocity.toFixed(2);
+  document.getElementById('velocity').textContent = formatter.format(velocity.toFixed(2));
   document.getElementById('altitude').textContent = altitude.toFixed(2);
 }
 
