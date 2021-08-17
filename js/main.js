c7d238e8691384b87a1047dd78f8f1b40594e84e
jlthompson96 <joeythompson1014@gmail.com>
2021-08-17 10:59:59 -0400

    Updated minor aspects of the documents

--- js/main.js
+++ js/main.js
@@ -18,25 +18,4 @@ window.onload = function() {
     };
     xmlhttp.open("GET","https://api.nasa.gov/planetary/apod?api_key=pn3mLYsxd3wPNvChD1zeLHzC5hUq7sA1xICB4eCp",true);
     xmlhttp.send();
-
-
-//Rover Photos
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var rover = JSON.parse(this.responseText);
-//         document.getElementById("roverTitle").innerHTML = rover.full_name;
-//             var imgs = new Image();
-//                 imgs.src = rover.img_src;
-//                 imgs.style.height = "34em";
-//                 imgs.style.width = "42em";
-//         document.getElementById("roverImage").appendChild(imgs);
-//         document.getElementById("imageDescription").innerHTML = rover.explanation;
-//         document.getElementById("imageDate").innerHTML = rover.date;  
-//     }
-// };
-// xmlhttp.open("GET","https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=pn3mLYsxd3wPNvChD1zeLHzC5hUq7sA1xICB4eCp",true);
-// xmlhttp.send();
-
-
 };
