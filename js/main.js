0ae6da800554149ac9a86ab6e3acfd1c400e2ae4
jlthompson96 <joeythompson1014@gmail.com>
2021-08-25 19:44:11 -0400

    Updated JS to handle if API sends YT video instead of image

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ window.onload = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             document.getElementById("title").innerHTML = myObj.title;
+            if(myObj.media_type == "image"){
                 var img = new Image();
                     img.src = myObj.hdurl;
                     img.style.height = "34em";
                     img.style.width = "42em";
-            document.getElementById("image").appendChild(img);
+            document.getElementById("image").appendChild(img); 
+            console.log(myObj.url);
+            }
+            else {
+                    let url = myObj.url
+                    document.getElementById("video").innerHTML += '<iframe src='+url+'></iframe>';
+            }
             document.getElementById("description").innerHTML = myObj.explanation;
             document.getElementById("date").innerHTML = myObj.date;
             document.getElementById("copyright").innerHTML = myObj.copyright;    
