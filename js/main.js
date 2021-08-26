e06c984dc5d3b31e62ee135b9dc37985944afd1e
Joey <joeythompson1014@gmail.com>
2021-08-26 10:10:19 -0400

    Fixed landing page issue and added countdown timer for user feedback.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 
 //NASA APOD
-window.onload = function() {
+window.onload = function () {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             document.getElementById("title").innerHTML = myObj.title;
-            if(myObj.media_type == "image"){
+            if (myObj.media_type == "image") {
                 var img = new Image();
-                    img.src = myObj.hdurl;
-                    img.style.height = "34em";
-                    img.style.width = "42em";
-            document.getElementById("image").appendChild(img); 
-            console.log(myObj.url);
+                img.src = myObj.hdurl;
+                img.style.height = "34em";
+                img.style.width = "42em";
+                document.getElementById("image").appendChild(img);
+                console.log(myObj.url);
             }
             else {
-                    let url = myObj.url
-                    document.getElementById("video").innerHTML += '<iframe src='+url+'></iframe>';
+                let url = myObj.url
+                document.getElementById("video").innerHTML += '<iframe src=' + url + '></iframe>';
             }
             document.getElementById("description").innerHTML = myObj.explanation;
             document.getElementById("date").innerHTML = myObj.date;
-            document.getElementById("copyright").innerHTML = myObj.copyright;    
+            document.getElementById("copyright").innerHTML = myObj.copyright;
         }
     };
-    xmlhttp.open("GET","https://api.nasa.gov/planetary/apod?api_key=pn3mLYsxd3wPNvChD1zeLHzC5hUq7sA1xICB4eCp",true);
+    xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=pn3mLYsxd3wPNvChD1zeLHzC5hUq7sA1xICB4eCp", true);
     xmlhttp.send();
 };
