92baa3d24e58483f91334d91491739d9cac863bf
Joey Thompson <joeythompson1014@gmail.com>
2021-08-26 12:17:45 -0400

    Removed "consol.log" statement and added a comment.

--- js/main.js
+++ js/main.js
@@ -6,13 +6,14 @@ window.onload = function () {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             document.getElementById("title").innerHTML = myObj.title;
+            
+            //Check if media is an image or video and output
             if (myObj.media_type == "image") {
                 var img = new Image();
                 img.src = myObj.hdurl;
                 img.style.height = "34em";
                 img.style.width = "42em";
                 document.getElementById("image").appendChild(img);
-                console.log(myObj.url);
             }
             else {
                 let url = myObj.url
