44252b05671f08044995502bc4c4a44302f1c930
Joey Thompson <joeythompson1014@gmail.com>
2024-03-29 21:04:20 -0400

    env test

--- src/components/APOD.tsx
+++ src/components/APOD.tsx
@@ -37,7 +37,7 @@ const NasaApod = () => {
     useEffect(() => {
         const fetchApod = async () => {
             try {
-                const NASA_API_KEY = process.env.NASA_API_KEY;
+                const NASA_API_KEY = process.env.VERCEL_NASA_API_KEY;
                 const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`);
                 if (!response.ok) {
                     throw new Error('Failed to fetch APOD');
